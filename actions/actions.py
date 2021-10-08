9cf3cbc288aa51b28c7c1a506a94edb686ce8070
carlad <carlad@Carlas-MacBook-Pro.local>
2021-10-08 15:52:24 +0200

    flake8 fixes, more interactive training

--- actions/actions.py
+++ actions/actions.py
@@ -13,30 +13,38 @@ from typing import Any, Dict, List, Text
 import requests
 import yaml
 
+
 class ActionSubmitQuery(Action):
+    """Format keywords and submits query to black history api."""
+
     def name(self) -> Text:
-        #define the name of the action		
+        """Name the action."""
         return 'action_submit_query'
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict,
-    ) -> List[Dict[Text, Any]]:
-    
-        #retrieve keywords slot value and normalize 
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: DomainDict) -> List[Dict[Text, Any]]:
+        """Normalize keywords and make request."""
+        #  retrieve keywords slot value and normalize
         keywords = tracker.get_slot('keyword')
-        query_string = '%20'.join([singularize(keyword) for keyword in keywords])
+        query_string = '%20'.join([singularize(keyword) for keyword in keywords]) # noqa
 
-        #retrieve black history api key		
+        #  retrieve black history api key
         with open('./bha_credentials.yml', 'r') as ymlfile:
             cfg = yaml.safe_load(ymlfile)
             key = cfg['credentials']['API_KEY']
 
-        headers = { 'x-api-key': key }
-				
-        #look for results using all the keywords
-        response = requests.get("https://rest.blackhistoryapi.io/fact/search/{}".format(query_string), headers=headers).json()
-        
-        if response['TotalResults']==0:
-            dispatcher.utter_message("Sorry, I didn't find anything for: {}. Try using different keywords or names.".format(' '.join(keywords)))
+        headers = {'x-api-key': key}
+
+        #  look for results using all the keywords
+        response = requests.get(
+                    "https://rest.blackhistoryapi.io/fact/search/{}"
+                    .format(query_string),
+                    headers=headers).json()
+
+        if response['TotalResults'] == 0:
+            dispatcher.utter_message(
+                """Sorry, I didn't find anything for: {}. Try using different keywords or names.""" # noqa
+                .format(' '.join(keywords)))
             return
 
         else:
@@ -44,5 +52,5 @@ class ActionSubmitQuery(Action):
             for i in response['Results']:
                 texts += i['text'] + '\n\n'
 
-        dispatcher.utter_message(texts) #send the response back to the user	
+        dispatcher.utter_message(texts)  # send the response back to the user
         return [SlotSet('keyword', 'none')]
