69e55f3f553fbde95836a66e5989346f6a3abd13
carlad <carlad@Carlas-MacBook-Pro.local>
2021-10-08 16:00:20 +0200

    remove caches

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,5 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
+"""Custom actions used to run custom Python code."""
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
