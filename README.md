6bf789ed1652925f838a79b5827e2e03483397e6
carlad <carlad@Carlas-MacBook-Pro.local>
2021-10-08 13:59:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Jackson is a chatbot that helps find historical facts about black people. It is named after Mary W. Jackson, the first African American female engineer at NASA.
+
+Jackson is built with [Rasa](https://github.com/RasaHQ/rasa), the open source machine learning framework that automates text-and voice-based conversations. Jackson uses the [Black History API](https://blackhistoryapi.com/), which documents achievements and facts about black people, past and present. 
+
+Jackson uses a simple keyword search.
+
+## Dependencies
+
+Python 3.8.6
+
+Jackson requires [rasa-sdk](https://github.com/RasaHQ/rasa-sdk) to run the custom search action:
+```
+pip install rasa-sdk
+```
+
+and [Pattern](https://github.com/clips/pattern) to singularize keywords:
+```
+pip install pattern
+```
+
+## To run
+
+To enable api calls to the Black History API you will need to [sign-up](https://blackhistoryapi.com/#signUp) for an api key from the Black History API, then add this key to a new file in the home directory named `bha_credentials.yml`
+
+```
+credentials:
+    API_KEY: XXXXXXXXXXXX
+```
+
+To talk to Jackson on the command line, start the Rasa Action Server in one terminal window: 
+```
+rasa run actions
+```
+
+Then in a new terminal window run `rasa shell`, and follow the prompts.
+
+## Further Help
+
+Please refer to the [Rasa Docs](https://rasa.com/docs/rasa/) for more information about Rasa Open Source.
---
+++ actions/actions.py
@@ -0,0 +1,48 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from pattern.text.en import singularize
+from typing import Any, Dict, List, Text
+import requests
+import yaml
+
+class ActionSubmitQuery(Action):
+    def name(self) -> Text:
+        #define the name of the action		
+        return 'action_submit_query'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict,
+    ) -> List[Dict[Text, Any]]:
+    
+        #retrieve keywords slot value and normalize 
+        keywords = tracker.get_slot('keyword')
+        query_string = '%20'.join([singularize(keyword) for keyword in keywords])
+
+        #retrieve black history api key		
+        with open('./bha_credentials.yml', 'r') as ymlfile:
+            cfg = yaml.safe_load(ymlfile)
+            key = cfg['credentials']['API_KEY']
+
+        headers = { 'x-api-key': key }
+				
+        #look for results using all the keywords
+        response = requests.get("https://rest.blackhistoryapi.io/fact/search/{}".format(query_string), headers=headers).json()
+        
+        if response['TotalResults']==0:
+            dispatcher.utter_message("Sorry, I didn't find anything for: {}. Try using different keywords or names.".format(' '.join(keywords)))
+            return
+
+        else:
+            texts = ''
+            for i in response['Results']:
+                texts += i['text'] + '\n\n'
+
+        dispatcher.utter_message(texts) #send the response back to the user	
+        return [SlotSet('keyword', 'none')]
