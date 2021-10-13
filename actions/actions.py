c54b4228bcc312ecee5d7ced96e5ed519de05de0
carlad <carlad@users.noreply.github.com>
2021-10-13 12:10:34 +0200

    refactor actions

--- actions/actions.py
+++ actions/actions.py
@@ -25,52 +25,50 @@ class ActionSubmitQuery(Action):
         """Normalize keywords and make request."""
         #  retrieve keywords slot value
         keywords = tracker.get_slot('keywords')
-        print(keywords)
-        print(keywords[0])
 
         # return if no keyword entities are detected
         if keywords is None:
             dispatcher.utter_message(
                 "Sorry, I wasn't able to understand your request. Try using different keywords or names.") # noqa
+            return [SlotSet('keywords', 'none')]
 
-        else:
-            # set headers
-            headers = {'x-api-key': bha_credentials.API_KEY}
+        # set headers
+        headers = {'x-api-key': bha_credentials.API_KEY}
 
-            # find a random fact
-            if keywords[0] == 'random' or keywords[0] == 'randomly':
-                response = requests.get(
-                        "https://rest.blackhistoryapi.io/fact/random",
-                        headers=headers).json()
+        # find a random fact
+        if keywords[0] == 'random' or keywords[0] == 'randomly':
+            response = requests.get(
+                    "https://rest.blackhistoryapi.io/fact/random",
+                    headers=headers).json()
 
-                text = response['Results'][0]['text']
+            text = response['Results'][0]['text']
 
-                dispatcher.utter_message(
-                        "Here's the random fact I found for you: \n{}.".format(text)) # noqa
+            dispatcher.utter_message(
+                    "Here's the random fact I found for you: \n{}.".format(text)) # noqa
+            return [SlotSet('keywords', 'none')]
 
-            else:
-                # normalize keywords
-                query_string = '%20'.join([singularize(keyword) for keyword in keywords]) # noqa
+        # normalize keywords
+        query_string = '%20'.join([singularize(keyword) for keyword in keywords]) # noqa
 
-                #  look for results using all the keywords
-                response = requests.get(
-                            "https://rest.blackhistoryapi.io/fact/search/{}"
-                            .format(query_string),
-                            headers=headers).json()
+        #  look for results using all the keywords
+        response = requests.get(
+                    "https://rest.blackhistoryapi.io/fact/search/{}"
+                    .format(query_string),
+                    headers=headers).json()
 
-                total_results = response['TotalResults']
+        total_results = response['TotalResults']
 
-                if total_results == 0:
-                    dispatcher.utter_message(
-                        "Sorry, I didn't find anything for: {}. Try using different keywords or names." # noqa
-                        .format(' '.join(keywords)))
+        if total_results == 0:
+            dispatcher.utter_message(
+                "Sorry, I didn't find anything for: {}. Try using different keywords or names." # noqa
+                .format(' '.join(keywords)))
+            return [SlotSet('keywords', 'none')]
 
-                else:
-                    fact_string = 'fact' if total_results == 1 else 'facts'
-                    texts = 'I found {} {}:\n'.format(total_results, fact_string) # noqa
-                    for i in response['Results']:
-                        texts += '{}\n'.format(i['text'])
+        fact_string = 'fact' if total_results == 1 else 'facts'
+        texts = 'I found {} {}:\n'.format(total_results, fact_string) # noqa
 
-                dispatcher.utter_message(texts)
+        for i in response['Results']:
+            texts += '{}\n'.format(i['text'])
+        dispatcher.utter_message(texts)
 
         return [SlotSet('keywords', 'none')]
